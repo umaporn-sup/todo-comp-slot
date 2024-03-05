9975d70606ae7fe9df8c046ad267bb2702cb1daa
umaporn-sup <umaporn@sit.kmutt.ac.th>
2024-03-05 16:07:36 +0700

    [add] todo app with scoped slot and props

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import TodoList from './components/TodoList.vue'
+import ProductList from './components/ProductList.vue'
+import todos from '../data/todos.json'
+</script>
+
+<template>
+  <div class="w-full">
+    <!-- <ProductList /> -->
+    <TodoList :todos="todos" />
+  </div>
+</template>
+
+<style scoped></style>
